823dd16d00c509aac83c4ffe4795e607c6bbade8
Muhammad Adil <chmuhammadadil7@gmail.com>
2021-09-03 22:05:59 +0500

    Web support, Added boxFit property, improved border stroke and fixed extra padding issue.

---
+++ example/lib/generated_plugin_registrant.dart
@@ -0,0 +1,15 @@
+//
+// Generated file. Do not edit.
+//
+
+// ignore_for_file: lines_longer_than_80_chars
+
+import 'package:circular_profile_avatar/circular_profile_avatar_web.dart';
+
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+// ignore: public_member_api_docs
+void registerPlugins(Registrar registrar) {
+  CircularProfileAvatarWeb.registerWith(registrar);
+  registrar.registerMessageHandler();
+}
